a41646d1de3917487a73e872dca4e4ddb94100c9
eremeevmaks41-sys <eremeevmaks41@gmail.com>
2026-09-25 09:16:02 +0300

    tenant testovuj: оглавление (300 постов, новых +300, ИИ-теги 156/300)

---
+++ docs/testovuj/config.js
@@ -0,0 +1,10 @@
+window.SZ_CONFIG = {
+  channel_name: "DemoTest",
+  channel_url: "https://t.me/testovuj",
+  channel_button: "Канал",
+  header_emoji: "📖",
+  bot_username: "oglavlenie_shop_bot",
+  licensee: "DemoTest",
+  license_key: "SZ-07664C891B74B580",
+  license_id: "trial-testovuj",
+};
